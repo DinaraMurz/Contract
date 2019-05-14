3276f2181ad9c9f9a1722f327e3534e2983c17a4
Доллар Музыка <dollarmudic@gmail.com>
2019-05-14 18:31:27 +0600

    fd

--- Contract/MainWindow.xaml.cs
+++ Contract/MainWindow.xaml.cs
@@ -20,9 +20,19 @@ namespace Contract
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void PrintButtonClick(object sender, RoutedEventArgs e)
+        {
+            PrintDialog printDialog = new PrintDialog();
+            if (printDialog.ShowDialog() == true)
+            {
+                printDialog.PrintVisual(contract, "Распечатываем Документ");
+            }
         }
     }
 }
